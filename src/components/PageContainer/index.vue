a571a3361c094df63e88a97a8c90c4785da1105d
roy <464485682@qq.com>
2022-06-24 11:35:47 +0800

    feat: page container

---
+++ src/components/PageContainer/index.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="page-container">
+        <div class="page-container-header">
+            <a-breadcrumb v-if="stack.length > 1" class="page-container-breadcrumb">
+                <a-breadcrumb-item v-for="(item, index) in stack" :key="index">
+                    <a v-if="item.path" :href="`/#${item.path}`">{{ item.meta ? item.meta.title : item.name }}</a>
+                    <span v-else>{{ item.meta ? item.meta.title : item.name }}</span>
+                </a-breadcrumb-item>
+            </a-breadcrumb>
+            <div class="page-container-header-heading">{{ route.meta ? route.meta.title : route.name }}</div>
+        </div>
+        <div class="page-container-content">
+            <slot />
+        </div>
+    </div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'PageContainer',
+});
+</script>
+<script setup>
+import { useRoute } from 'vue-router';
+import routes from '@/router/routes';
+
+const route = useRoute();
+
+const stack = [];
+let flag = false;
+
+const findRoute = (routeList, path) => {
+    for (let i = 0; i < routeList.length; i++) {
+        const { path: routePath, children, name, meta } = routeList[i];
+        stack.push({
+            path: routePath,
+            name,
+            meta,
+        });
+        if (routeList[i].path === path) {
+            flag = true;
+            return;
+        }
+        if (children) {
+            findRoute(children, path);
+        }
+        if (flag) {
+            return;
+        }
+        stack.pop();
+    }
+};
+
+findRoute(routes[0].children, route.path);
+
+</script>
+<style scoped lang="less" src="./index.less"></style>
