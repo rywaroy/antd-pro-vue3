c6b2c4fbbcd419baa4ef9ff150ca8362353d0794
roy <464485682@qq.com>
2022-06-28 11:26:14 +0800

    feat: svg图标组件

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <svg class="svg-icon" aria-hidden="true" v-on="$attrs">
+        <use :xlink:href="`#icon-${name}`" />
+    </svg>
+</template>
+<script setup>
+
+defineProps({
+    name: String,
+});
+
+</script>
+
+<style scoped>
+.svg-icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
