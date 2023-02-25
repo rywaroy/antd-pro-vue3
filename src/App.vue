37aeab741e2f9e81f382d91a205f24a10012b041
roy <464485682@qq.com>
2023-02-25 15:33:13 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
 <template>
-    <a-config-provider :locale="locale">
+    <a-config-provider :locale="zhCN">
         <router-view />
     </a-config-provider>
 </template>
-<script>
+<script setup>
 import zhCN from 'ant-design-vue/es/locale/zh_CN';
 import dayjs from 'dayjs';
 import 'dayjs/locale/zh-cn';
 
 dayjs.locale('zh-cn');
 
-export default {
-    data() {
-        return {
-            locale: zhCN,
-        };
-    },
-};
 </script>
