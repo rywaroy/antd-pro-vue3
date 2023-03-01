acbf2e0a8f73998c473a0bb6fe933d87708cee12
roy <464485682@qq.com>
2023-03-01 14:31:18 +0800

    feat: 标签页样式优化

--- src/components/BasePageTab/index.vue
+++ src/components/BasePageTab/index.vue
@@ -1,42 +1,44 @@
 <template>
-    <a-tabs
-        v-model:activeKey="active"
-        hide-add
-        type="editable-card"
-        class="base-page-tab"
-        @change="changeTab"
-        @edit="editTab">
-        <template #rightExtra>
-            <a-dropdown>
-                <div class="base-page-tab-extra">
-                    <ellipsis-outlined />
-                </div>
-                <template #overlay>
-                    <a-menu>
-                        <a-menu-item @click="closeOtherTabs()">关闭其他</a-menu-item>
-                        <a-menu-item @click="handleReload">刷新当前页</a-menu-item>
-                    </a-menu>
-                </template>
-            </a-dropdown>
-        </template>
-        <a-tab-pane v-for="(tab, index) in tabs" :key="tab.name" :closable="tab.name !== 'Home'">
-            <template #tab>
-                <a-dropdown :trigger="['contextmenu']">
-                    <span>
-                        {{ tab.meta.title }}
-                        <reload-outlined v-if="tab.name === active" class="reload" :class="{ loading: reloadLoading }" @click="handleReload" />
-                    </span>
+    <div class="base-page-tab-wrap">
+        <a-tabs
+            v-model:activeKey="active"
+            hide-add
+            type="editable-card"
+            class="base-page-tab"
+            @change="changeTab"
+            @edit="editTab">
+            <template #rightExtra>
+                <a-dropdown>
+                    <div class="base-page-tab-extra">
+                        <ellipsis-outlined />
+                    </div>
                     <template #overlay>
                         <a-menu>
-                            <a-menu-item @click="closeOtherTabs(index)">关闭其他</a-menu-item>
-                            <a-menu-item :disabled="index === tabs.length - 1" @click="closeRightTabs(index)">关闭到右侧</a-menu-item>
-                            <a-menu-item :disabled="tab.name !== active" @click="handleReload">刷新当前页</a-menu-item>
+                            <a-menu-item @click="closeOtherTabs()">关闭其他</a-menu-item>
+                            <a-menu-item @click="handleReload">刷新当前页</a-menu-item>
                         </a-menu>
                     </template>
                 </a-dropdown>
             </template>
-        </a-tab-pane>
-    </a-tabs>
+            <a-tab-pane v-for="(tab, index) in tabs" :key="tab.name" :closable="tab.name !== 'Home'">
+                <template #tab>
+                    <a-dropdown :trigger="['contextmenu']">
+                        <span>
+                            {{ tab.meta.title }}
+                            <reload-outlined v-if="tab.name === active" class="reload" :class="{ loading: reloadLoading }" @click="handleReload" />
+                        </span>
+                        <template #overlay>
+                            <a-menu>
+                                <a-menu-item @click="closeOtherTabs(index)">关闭其他</a-menu-item>
+                                <a-menu-item :disabled="index === tabs.length - 1" @click="closeRightTabs(index)">关闭到右侧</a-menu-item>
+                                <a-menu-item :disabled="tab.name !== active" @click="handleReload">刷新当前页</a-menu-item>
+                            </a-menu>
+                        </template>
+                    </a-dropdown>
+                </template>
+            </a-tab-pane>
+        </a-tabs>
+    </div>
 </template>
 <script>
 import { defineComponent, inject, ref } from 'vue';
